5fdc6a85d3c12d791faeb26278a59181c9cfb5fc
StanislavFedyna <stanislavfedyna@gmail.com>
2018-08-06 16:53:25 +0300

    added cache functionality for img.js, added source map, update

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,17 +2,20 @@ var
 sass         = require('gulp-sass'),
 csscomb      = require('gulp-csscomb'),
 browserSync  = require('browser-sync'), 
-autoprefixer = require('gulp-autoprefixer');
+autoprefixer = require('gulp-autoprefixer'),
+sourcemaps   = require('gulp-sourcemaps');
 
 module.exports = function (gulp, plugins) {
   
   return function () {
     gulp.src('src/sass/*.scss')
+      .pipe(sourcemaps.init())
       .pipe(csscomb())
       .pipe(sass())
       .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
         cascade: true
       }))
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest('src/css'))
       .pipe(browserSync.reload({
         stream: true
